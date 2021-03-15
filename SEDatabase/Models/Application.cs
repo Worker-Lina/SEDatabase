b9f179bab7c24152cf4520eb6ef57cb460e68b97
Anuar <beisekeyev.a.1992.ast@gmail.com>
2021-03-15 21:45:25 +0600

    Добавил классы Technic, Application

---
+++ SEDatabase/Models/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDatabase.Models
+{
+    public class Application
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Status Status { get; set; }
+        public int TechnicId { get; set; }
+        public int Price { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
---
+++ SEDatabase/Models/Technic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDatabase.Models
+{
+    public class Technic
+    {    
+        public string Model { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public string Description { get; set; }
+    }
+}
